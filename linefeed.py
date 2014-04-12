876c1dab6474dd9eb30f0d1d2c6c823429800bc9
Peng <yangpeng.xmu@foxmail.com>
2014-04-13 00:21:54 +0800

    解析歌词每行的时间

--- linefeed.py
+++ linefeed.py
@@ -1,22 +1,38 @@
 __author__ = 'Peng'
-
+from datetime import timedelta
 
 def count_word(text):
     wlist = text.split()
     return len(wlist)
 
 
-def line_feed(text, width=40, whitespace=' '):
-    rtn = ''
+def line_feed(text, width=40):
+    rtn=[]
     while len(text) > width:
-        rtn += text[:width]
+        line = text[:width]
         text = text[width:]
-        if text[0] not in whitespace:
-            i = 1
-            while text[i] not in whitespace:
-                i += 1
-            rtn += text[:i]
-            text = text[i:]
-        rtn += '\n'
-    rtn += text
+        #如果这一行第width个字符是字母或数字
+        if str(line[-1]).isalnum():
+            for i, s in enumerate(reversed(line[:-1])):
+                if not s.isalnum():
+                    line, text = line[:-i - 1], line[-i - 1:] + text
+                    break
+        rtn.append(line)
+    rtn.append(text)
     return rtn
+
+def seperate_time(time1,time2,piece):
+    delta=(time2-time1)/(piece-1)
+    timelist=[]
+    for i in range(piece):
+        timelist.append(time1+delta*i)
+    return timelist
+
+def convert_to_timedelta(stime,brackets=True):
+    if brackets:
+        stime=stime[1:-1]
+    min,sec=str(stime).split(sep=':')
+    min=int(min)
+    sec=float(sec)
+    delta=timedelta(minutes=min,seconds=sec)
+    return delta
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 __author__ = 'Peng'
-from linefeed import count_word,line_feed
+from linefeed import *
 
 text='[00:00.49]The Moon is likely to become the industrial hub of the Solar System, supplying the rocket fuels fro its ships, easily obtainable from the lunar rocks in the from of liquid oxygen.'
-print(count_word(text))
-print(line_feed(text))
+
+lst=line_feed(text)
+print('\n'.join(lst))
+
+t1=convert_to_timedelta('[01:00.49]')
+t2=convert_to_timedelta('[01:03.49]')
+sep=seperate_time(t1,t2,3)
+print(sep)
