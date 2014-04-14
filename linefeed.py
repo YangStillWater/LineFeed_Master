4817c5a29190effb4fcfbb9d2501f8a18454416e
Peng <yangpeng.xmu@foxmail.com>
2014-04-14 23:47:28 +0800

    可以对一个歌词文件进行换行处理

--- linefeed.py
+++ linefeed.py
@@ -1,13 +1,14 @@
 __author__ = 'Peng'
 from datetime import timedelta
 
+
 def count_word(text):
     wlist = text.split()
     return len(wlist)
 
 
 def line_feed(text, width=40):
-    rtn=[]
+    rtn = []
     while len(text) > width:
         line = text[:width]
         text = text[width:]
@@ -21,26 +22,58 @@ def line_feed(text, width=40):
     rtn.append(text)
     return rtn
 
-def seperate_time(time1,time2,piece):
-    delta=(time2-time1)/(piece-1)
-    timelist=[]
+
+def seperate_time(time1, time2, piece):
+    if time2 and piece>1:
+        delta = (time2 - time1) / piece
+    else:
+        delta = timedelta(seconds=1)
+    timelist = []
     for i in range(piece):
-        timelist.append(time1+delta*i)
+        timelist.append(time1 + delta * i)
     return timelist
 
-def string_to_timedelta(stime,brackets=True):
+
+def string_to_timedelta(stime, brackets=True):
     if brackets:
-        stime=stime[1:-1]
-    min,sec=str(stime).split(sep=':')
-    min=int(min)
-    sec=float(sec)
-    delta=timedelta(minutes=min,seconds=sec)
+        stime = stime[1:-1]
+    min, sec = str(stime).split(sep=':')
+    min = int(min)
+    sec = float(sec)
+    delta = timedelta(minutes=min, seconds=sec)
     return delta
 
+
 def timedelta_to_string(delta):
-    min,sec=divmod(delta.seconds,60)
-    min='%02d'%min
-    sec='{:02d}.{:02d}'.format(sec,int(delta.microseconds/10000))
-    rtn='[{}:{}]'.format(min,sec)
+    min, sec = divmod(delta.seconds, 60)
+    min = '%02d' % min
+    sec = '{:02d}.{:02d}'.format(sec, int(delta.microseconds / 10000))
+    rtn = '[{}:{}]'.format(min, sec)
     return rtn
 
+
+def split_lrc(lrc1, lrc2,width):
+    time1 = string_to_timedelta(lrc1[:10])
+    if lrc2:
+        time2 = string_to_timedelta(lrc2[:10])
+    else:
+        time2 = None
+    lrc1 = lrc1[10:]
+    lrclist = line_feed(lrc1,width)
+    timelist = seperate_time(time1, time2, len(lrclist))
+    rtnlist = []
+    for i, l in enumerate(lrclist):
+        s = timedelta_to_string(timelist[i]) + l
+        rtnlist.append(s)
+
+    return rtnlist
+
+def feed_lrc(filename,width=40):
+    f=open(filename,'r')
+    lst=[]
+    l1=f.readline()
+    while l1:
+        l2=f.readline()
+        lst+=split_lrc(l1,l2,width)
+        l1=l2
+    return  lst
--- test.py
+++ test.py
@@ -1,14 +1,22 @@
 __author__ = 'Peng'
 from linefeed import *
 
-# text='[00:00.49]The Moon is likely to become the industrial hub of the Solar System, supplying the rocket fuels fro its ships, easily obtainable from the lunar rocks in the from of liquid oxygen.'
-#
-# lst=line_feed(text)
-# print('\n'.join(lst))
+text='[00:00.49]The Moon is likely to become the industrial hub of the Solar System, supplying the rocket fuels fro its ships, easily obtainable from the lunar rocks in the from of liquid oxygen.'
 
-t1=string_to_timedelta('[01:00.49]')
-t2=string_to_timedelta('[01:03.49]')
-sep=seperate_time(t1,t2,3)
-print(sep)
-s=timedelta_to_string(t1)
+# lst=split_lrc(text)
+# print('\n'.join(lst))
+#
+# t1=string_to_timedelta('[01:00.49]')
+# t2=string_to_timedelta('[01:03.49]')
+# sep=seperate_time(t1,t2,3)
+# print(sep)
+# s=timedelta_to_string(t1)
+# print(s)
+
+lst=feed_lrc('test.lrc',50)
+s='\n'.join(lst)
+f=open('output.lrc','w')
+f.write(s)
 print(s)
+
+
